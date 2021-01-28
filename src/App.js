035f066223f5b3cdae7e2a82e814512960c718df
Emmanuel Tesauro <emmanuel.tesauro@gmail.com>
2021-01-28 13:25:55 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from "./component/Navigation";
+import Header from "./component/Header";
+import Stack from "./component/Stack";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Header />
+      <Stack />
+    </div>
+  );
+}
+
+export default App;
